8ee6578a82cbfc494ca8076d1ad5e5fade9377bf
Lynn <65545348+lynnswap@users.noreply.github.com>
2025-05-20 15:50:10 +0900

    Add configurable pan gesture

--- Sources/PDVideoPlayer/Player/PDVideoPlayerProxy+Extensions.swift
+++ Sources/PDVideoPlayer/Player/PDVideoPlayerProxy+Extensions.swift
@@ -4,10 +4,14 @@ import SwiftUI
 @MainActor
 extension PDVideoPlayerProxy {
     func player(
+        panGesture: PDVideoPlayerPanGesture? = nil,
         scrollViewConfigurator: PDVideoPlayerRepresentable.ScrollViewConfigurator? = nil,
         contextMenuProvider: PDVideoPlayerRepresentable.ContextMenuProvider? = nil
     ) -> PDVideoPlayerRepresentable {
         var view = self.player
+        if let panGesture {
+            view = view.panGesture(panGesture)
+        }
         if let scrollViewConfigurator {
             view = view.scrollViewConfigurator(scrollViewConfigurator)
         }
