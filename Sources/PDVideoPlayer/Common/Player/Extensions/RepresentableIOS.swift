e28d3b48d04ea5d5dc9a3c5d9dcf21cb57bbc930
Lynn <65545348+lynnswap@users.noreply.github.com>
2025-05-23 15:26:59 +0900

    Clarify presentation size modifier description

--- Sources/PDVideoPlayer/Common/Player/Extensions/RepresentableIOS.swift
+++ Sources/PDVideoPlayer/Common/Player/Extensions/RepresentableIOS.swift
@@ -3,15 +3,39 @@ import SwiftUI
 
 public extension PDVideoPlayerRepresentable {
     func scrollViewConfigurator(_ configurator: @escaping ScrollViewConfigurator) -> Self {
-        Self(model: self.model, closeGesture: self.closeGesture, scrollViewConfigurator: configurator, contextMenuProvider: self.contextMenuProvider, onTap: self.onTap)
+        Self(model: self.model,
+             closeGesture: self.closeGesture,
+             scrollViewConfigurator: configurator,
+             contextMenuProvider: self.contextMenuProvider,
+             onPresentationSizeChange: self.onPresentationSizeChange,
+             onTap: self.onTap)
     }
 
     func contextMenuProvider(_ provider: @escaping ContextMenuProvider) -> Self {
-        Self(model: self.model, closeGesture: self.closeGesture, scrollViewConfigurator: self.scrollViewConfigurator, contextMenuProvider: provider, onTap: self.onTap)
+        Self(model: self.model,
+             closeGesture: self.closeGesture,
+             scrollViewConfigurator: self.scrollViewConfigurator,
+             contextMenuProvider: provider,
+             onPresentationSizeChange: self.onPresentationSizeChange,
+             onTap: self.onTap)
     }
 
     func closeGesture(_ gesture: PDVideoPlayerCloseGesture) -> Self {
-        Self(model: self.model, closeGesture: gesture, scrollViewConfigurator: self.scrollViewConfigurator, contextMenuProvider: self.contextMenuProvider, onTap: self.onTap)
+        Self(model: self.model,
+             closeGesture: gesture,
+             scrollViewConfigurator: self.scrollViewConfigurator,
+             contextMenuProvider: self.contextMenuProvider,
+             onPresentationSizeChange: self.onPresentationSizeChange,
+             onTap: self.onTap)
+    }
+
+    func onPresentationSizeChange(_ action: @escaping PresentationSizeAction) -> Self {
+        Self(model: self.model,
+             closeGesture: self.closeGesture,
+             scrollViewConfigurator: self.scrollViewConfigurator,
+             contextMenuProvider: self.contextMenuProvider,
+             onPresentationSizeChange: action,
+             onTap: self.onTap)
     }
 }
 #endif
