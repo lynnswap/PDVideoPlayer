b62ace4b4bfc136f36dbb64d4cea33932c432058
Lynn <65545348+lynnswap@users.noreply.github.com>
2025-05-23 15:03:55 +0900

    Rename tapAction modifier to onTap

--- Sources/PDVideoPlayer/Common/Player/Extensions/TapAction.swift
+++ Sources/PDVideoPlayer/Common/Player/Extensions/TapAction.swift
@@ -1,45 +1,30 @@
 import SwiftUI
 
 public extension PDVideoPlayerRepresentable {
-    func tapAction(_ action: VideoPlayerTapAction) -> Self {
+    func onTap(_ action: VideoPlayerTapAction) -> Self {
         #if os(iOS)
         Self(model: self.model,
              panGesture: self.panGesture,
              scrollViewConfigurator: self.scrollViewConfigurator,
              contextMenuProvider: self.contextMenuProvider,
-             tapAction: action)
+             onTap: action)
         #elseif os(macOS)
         Self(model: self.model,
              playerViewConfigurator: self.playerViewConfigurator,
              onPresentationSizeChange: self.onPresentationSizeChange,
-             tapAction: action,
+             onTap: action,
              menuContent: self.menuContent)
         #else
         self
         #endif
     }
 
-    func tapAction(_ action: @escaping (Bool) -> Void) -> Self {
-        tapAction(VideoPlayerTapAction(action))
-    }
-
-    func tapAction(_ action: @escaping () -> Void) -> Self {
-        tapAction { _ in action() }
-    }
-
-    /// SwiftUI-style alias for ``tapAction(_:)``.
-    func onTap(_ action: VideoPlayerTapAction) -> Self {
-        tapAction(action)
-    }
-
-    /// SwiftUI-style alias for ``tapAction(_:)``.
     func onTap(_ action: @escaping (Bool) -> Void) -> Self {
-        tapAction(action)
+        onTap(VideoPlayerTapAction(action))
     }
 
-    /// SwiftUI-style alias for ``tapAction(_:)``.
     func onTap(_ action: @escaping () -> Void) -> Self {
-        tapAction(action)
+        onTap { _ in action() }
     }
 }
 
