3c47216aae5929012debd99a1bd4219a0c6c54e3
Lynn <65545348+lynnswap@users.noreply.github.com>
2025-05-23 14:58:32 +0900

    Rename onResize to onPresentationSizeChange

--- Sources/PDVideoPlayer/Common/Player/Extensions/RepresentableMac.swift
+++ Sources/PDVideoPlayer/Common/Player/Extensions/RepresentableMac.swift
@@ -3,11 +3,11 @@ import SwiftUI
 
 public extension PDVideoPlayerRepresentable {
     func playerViewConfigurator(_ configurator: @escaping PlayerViewConfigurator) -> Self {
-        Self(model: self.model, playerViewConfigurator: configurator, onResize: self.onResize, tapAction: self.tapAction, menuContent: self.menuContent)
+        Self(model: self.model, playerViewConfigurator: configurator, onPresentationSizeChange: self.onPresentationSizeChange, tapAction: self.tapAction, menuContent: self.menuContent)
     }
 
-    func onResize(_ action: @escaping ResizeAction) -> Self {
-        Self(model: self.model, playerViewConfigurator: self.playerViewConfigurator, onResize: action, tapAction: self.tapAction, menuContent: self.menuContent)
+    func onPresentationSizeChange(_ action: @escaping PresentationSizeAction) -> Self {
+        Self(model: self.model, playerViewConfigurator: self.playerViewConfigurator, onPresentationSizeChange: action, tapAction: self.tapAction, menuContent: self.menuContent)
     }
 }
 #endif
