18fc3e1cf79c60610a46402f186e99a47efb855d
lynnswap <65545348+lynnswap@users.noreply.github.com>
2025-05-30 18:13:52 +0900

    bug fixes

--- Sources/PDVideoPlayer/Common/PlaybackSpeedMenuView.swift
+++ Sources/PDVideoPlayer/Common/PlaybackSpeedMenuView.swift
@@ -4,11 +4,11 @@ import SwiftUI
 /// A menu allowing selection of playback speed.
 public struct PlaybackSpeedMenuView: View {
     @Environment(PDPlayerModel.self) private var model
-
+    @Environment(\.videoPlayerPlaybackSpeed) private var speedBinding
     public init() {}
 
     public var body: some View {
-        Picker(selection: Bindable(model).playbackSpeed) {
+        Picker(selection: speedBinding ?? Bindable(model).playbackSpeed) {
             ForEach(PlaybackSpeed.allCases) { speed in
                 Text(speed.displayName).tag(speed)
             }
