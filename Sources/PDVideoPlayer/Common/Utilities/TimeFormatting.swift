7a1c19ea136b081724762c69287a3560bdfae23e
lynnswap <65545348+lynnswap@users.noreply.github.com>
2025-07-19 19:53:38 +0900

    fixes

--- Sources/PDVideoPlayer/Common/Utilities/TimeFormatting.swift
+++ Sources/PDVideoPlayer/Common/Utilities/TimeFormatting.swift
@@ -2,10 +2,10 @@ import Foundation
 
 extension Double {
     var mmSSString: String {
-        guard self.isFinite && self >= 0 else { return "00:00" }
+        guard self.isFinite && self >= 0 else { return "0:00" }
         let totalSec = Int(self)
         let minutes = totalSec / 60
         let seconds = totalSec % 60
-        return String(format: "%02d:%02d", minutes, seconds)
+        return String(format: "%01d:%02d", minutes, seconds)
     }
 }
